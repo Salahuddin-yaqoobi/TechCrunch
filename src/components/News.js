3dd34c2e2097335c9fcd4813525aea4f6d13d766
Salah ud din <salahuddin5432112345@gmail.com>
2024-08-20 15:52:12 +0500

    Upto Pre and Next btn

---
+++ src/components/News.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+
+export class News extends Component { 
+
+  constructor() {
+    super();
+    this.state = {
+      articles:  [],
+      loading: false,
+      page:1
+    };
+  }
+  async componentDidMount(){
+    console.log("i am using the cdm");
+    let url = "https://newsapi.org/v2/everything?q=apple&q=tesla&q=microsoft&from=2024-08-18&to=2024-08-18&sortBy=popularity&apiKey=f73b98a7cd2b4da5abfa54d5df8fbd03&page=1&pagesize=20";
+    let data = await fetch(url);
+  
+    // let parsedData = await data.json();
+    // console.log(parsedData);
+    // this.setState = ({
+      // articles : parsedData.articles
+    // })
+  
+     await fetch(url).then((res) => res.json())
+                .then((json) => {
+                    this.setState({
+                        articles: json.articles,
+                        totalResult : json.totalResult,
+                        loading: false,
+                        page :1
+                    });
+                })
+  }
+
+  handleNextBtn =  async ()=>{
+    if(this.state.page +1 > (Math.ceil(this.totalResult/20))){
+
+    }
+    else{
+    console.log("i am using the cdm");
+    let url = `https://newsapi.org/v2/everything?q=apple&q=tesla&q=microsoft&language=en&from=2024-08-18&to=2024-08-18&sortBy=popularity&apiKey=f73b98a7cd2b4da5abfa54d5df8fbd03&page=${this.state.page + 1}&pagesize=20`;
+    let data = await fetch(url);
+  
+        console.log("click Next button");
+        await fetch(url).then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                articles: json.articles,
+                loading: false,
+                page : this.state.page + 1
+            });
+        })
+      }
+  }
+  handlePrevBtn = async ()=>{
+    console.log("click Next button")
+    console.log("i am using the cdm");
+    let url = `https://newsapi.org/v2/everything?q=apple&q=tesla&q=microsoft&from=2024-08-18&to=2024-08-18&sortBy=popularity&apiKey=f73b98a7cd2b4da5abfa54d5df8fbd03&page=${this.state.page - 1}&pagesize=20`;
+    let data = await fetch(url);
+  
+        console.log("click Next button");
+        await fetch(url).then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                articles: json.articles,
+                loading: false,
+                page : this.state.page - 1
+            });
+        })
+ 
+  }
+   render() {
+    return (
+      <div className="container my-4">
+        <h2>TechCrunch - Top Headlines</h2>
+        <div className="row">
+         {this.state.articles.map((element) =>{
+             return <div className="col-md-4" key={element.url}>
+             <Newsitem
+               title={element.title?element.title.slice(0,45):""}
+               description={element.description?element.description.slice(0,88):""}
+               imageUrl={element.urlToImage}
+               newsUrl = {element.url}
+             />
+           </div>          
+          })
+        }
+        </div>
+        <div className="conatainer d-flex justify-content-between">
+        <button disabled = {this.state.page<=1} type="button" onClick={this.handlePrevBtn} className="btn btn-dark">&larr; Prevoius</button>
+        <button type="button" onClick={this.handleNextBtn} className="btn btn-dark">Next &rarr;</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
